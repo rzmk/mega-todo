809d44450200c0f7782f1e17439a121c65c347bf
Ishaan Bedi <bediishaan6@gmail.com>
2022-01-04 10:32:11 +0530

    Updated the webpage with all the required features
    Updated the webpage with all the required features including button to add rows.
    Also updated CSS file and Readme file.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-const table = document.getElementById('assignmentsTable');
-let baseRow = document.getElementById("baseRow");
+const table = document.querySelector('#assignmentsTable');
+const buttonInsert = document.querySelector('.buttonInsert');
+var allInputs = document.querySelectorAll('input');
+var rowNo = 2;
+buttonInsert.addEventListener('click', function () {
+    var row = table.insertRow(rowNo)
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
 
-// Add <tr> to table
-table.append(baseRow);
+    cell1.innerHTML = document.querySelector('.assignmentName').value;
+    cell2.innerHTML = document.querySelector('.className').value;
+    cell3.innerHTML = document.querySelector('.dateinput').value;
+    cell4.innerHTML = document.querySelector('.timeinput').value;
+    cell5.innerHTML = document.querySelector('.priorityInput').value;
+    cell6.innerHTML = document.querySelector('.estimatedTimeInput').value;
+    cell7.innerHTML = document.querySelector('.status').value;
+    document.querySelector('.assignmentName').value ='';
+    document.querySelector('.className').value ='';
+    document.querySelector('.dateinput').value ='';
+    document.querySelector('.timeinput').value ='';
+    document.querySelector('.priorityInput').value ='';
+    document.querySelector('.estimatedTimeInput').value ='';
+    document.querySelector('.status').value ='';
+    
+    rowNo++;
+})
+
+
